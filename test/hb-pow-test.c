618635d1118d21e278f8a1ce34c5d5728dae33fb
Connor Imes <connor.k.imes@gmail.com>
2015-11-24 16:55:57 -0600

    Run basic tests during build and add more complete unit tests. Fixes #9.

--- test/hb-pow-test.c
+++ test/hb-pow-test.c
@@ -1,6 +1,7 @@
 /**
  * Just tests that the functions are all there.
  */
+#include <assert.h>
 #include <inttypes.h>
 #include <stdlib.h>
 #include "heartbeat-pow.h"
@@ -9,6 +10,7 @@ int main(int argc, char** argv) {
   uint64_t window_size = 20;
   heartbeat_pow_context hb;
   heartbeat_pow_record* window_buffer = malloc(window_size * sizeof(heartbeat_pow_record));
+  assert(window_buffer);
   heartbeat_pow_init(&hb, window_size, window_buffer, -1, NULL);
   heartbeat_pow(&hb, 0, 1, 0, 1000000000, 0, 1000000);
   hb_pow_log_header(1);
